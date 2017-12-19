60e027572513352fc79ce3367f937de782b0b48b
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2017-12-19 12:37:51 +0300

    Field class - returned to initial state

--- source/MNSWPR/MNSWPR.Core/Field.cs
+++ source/MNSWPR/MNSWPR.Core/Field.cs
@@ -10,22 +10,5 @@ namespace MNSWPR.Core
     {
         private int rows;
         private int cols;
-
-        private Cell[] minedCells;
-
-        public Field()
-        {
-            this.rows = 5;
-            this.cols = 5;
-
-            minedCells = new Cell[5]
-            {
-                new Cell(0, 4, 5),
-                new Cell(1, 2, 5),
-                new Cell(2, 4, 5),
-                new Cell(3, 3, 5),
-                new Cell(4, 1, 5)
-            };
-        }
     }
 }
