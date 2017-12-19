5b29af23fb8c2514471dac775f609e4c6c8f0b9e
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2017-12-19 13:00:42 +0300

    Field initializing; randomizer

--- source/MNSWPR/MNSWPR.Core/Field.cs
+++ source/MNSWPR/MNSWPR.Core/Field.cs
@@ -10,5 +10,27 @@ namespace MNSWPR.Core
     {
         private int rows;
         private int cols;
+
+        //keep ALL cells (not only mined) to access cell directly ( O(1) operation)
+        //true if mined
+        private bool[] cells;
+
+        public Field(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            cells = new bool[rows * cols];
+            SetMines();
+        }
+
+        private void SetMines()
+        {
+            var r = new Randomizer();
+            var mineIndexes = r.GetRandomNumbers(5, rows * cols);
+            foreach(var index in mineIndexes)
+            {
+                cells[index] = true;
+            }
+        }
     }
 }
---
+++ source/MNSWPR/MNSWPR.Core/Randomizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNSWPR.Core
+{
+    public class Randomizer
+    {
+        public int[] GetRandomNumbers(int count, int maxValue)
+        {
+            var numbers = new int[count];
+            var r = new Random(DateTime.Now.GetHashCode());
+            for(var i = 0; i < count; i++)
+            {
+                numbers[i] = r.Next(maxValue);
+            }
+            return numbers;
+        }
+    }
+}
