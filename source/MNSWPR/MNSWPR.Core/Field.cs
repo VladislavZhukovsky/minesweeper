ac8a70378af42e62f28b3a592d70c97179a62864
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2017-12-20 16:10:24 +0300

    Cell coordinates validation; Counting mines around implemented

--- source/MNSWPR/MNSWPR.Core/Field.cs
+++ source/MNSWPR/MNSWPR.Core/Field.cs
@@ -59,16 +59,28 @@ namespace MNSWPR.Core
             Ncol = Scol = col;
             NEcol = Ecol = SEcol = col + 1;
 
-            if (NWrow >= 0 && NWcol >= 0) { if (Mined(NWrow, NWcol)) { result++; } }
-            if (Nrow  >= 0 && Ncol  >= 0) { if (Mined(Nrow,  Ncol))  { result++; } }
-            if (NErow >= 0 && NEcol >= 0) { if (Mined(NErow, NEcol)) { result++; } }
-            if (Erow  >= 0 && Ecol  >= 0) { if (Mined(Erow,  Ecol))  { result++; } }
-            if (SErow >= 0 && SEcol >= 0) { if (Mined(SErow, SEcol)) { result++; } }
-            if (Srow  >= 0 && Scol  >= 0) { if (Mined(Srow,  Scol))  { result++; } }
-            if (SWrow >= 0 && SWcol >= 0) { if (Mined(SWrow, SWcol)) { result++; } }
-            if (Wrow  >= 0 && Wcol  >= 0) { if (Mined(Wrow,  Wcol))  { result++; } }
+            if (IsValidCellCoordinates(NWrow,NWcol)) { if (Mined(NWrow, NWcol)) { result++; } }
+            if (IsValidCellCoordinates(Nrow ,Ncol )) { if (Mined(Nrow,  Ncol))  { result++; } }
+            if (IsValidCellCoordinates(NErow,NEcol)) { if (Mined(NErow, NEcol)) { result++; } }
+            if (IsValidCellCoordinates(Erow ,Ecol )) { if (Mined(Erow,  Ecol))  { result++; } }
+            if (IsValidCellCoordinates(SErow,SEcol)) { if (Mined(SErow, SEcol)) { result++; } }
+            if (IsValidCellCoordinates(Srow ,Scol )) { if (Mined(Srow,  Scol))  { result++; } }
+            if (IsValidCellCoordinates(SWrow,SWcol)) { if (Mined(SWrow, SWcol)) { result++; } }
+            if (IsValidCellCoordinates(Wrow ,Wcol )) { if (Mined(Wrow,  Wcol))  { result++; } }
 
             return result;
+        }
+
+        /// <summary>
+        /// Checks if the value is in specified range (including min and max value)
+        /// </summary>
+        /// <param name="value">Value to check</param>
+        /// <param name="min">Beginning of range</param>
+        /// <param name="max">End of range</param>
+        /// <returns></returns>
+        private bool IsValidCellCoordinates(int row, int col)
+        {
+            return !(row < 0 || row >= rows || col < 0 || col >= cols);
         }
     }
 }
--- source/MNSWPR/MNSWPR.TestApp/Program.cs
+++ source/MNSWPR/MNSWPR.TestApp/Program.cs
@@ -12,8 +12,20 @@ namespace MNSWPR.TestApp
     {
         static void Main(string[] args)
         {
-            var field = new Field(5, 5);
+            //var rows = 5;
+            //var field = new Field(rows, rows);
 
+            //for (var i = 0; i < rows; i++)
+            //{
+            //    for(var j = 0; j < rows; j++)
+            //    {
+            //        if (field.Mined(i, j))
+            //            Console.Write(1);
+            //        else
+            //            Console.Write(0);
+            //    }
+            //    Console.WriteLine();
+            //}
         }
     }
 }
